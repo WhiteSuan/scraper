df48277aec07e5000fc4a5750c6634087970f940
whitenguyen <whitenguyen2512@gmail.com>
2018-10-09 21:17:06 +0700

    jsscraper ver4.1

--- api/views.py
+++ api/views.py
@@ -11,13 +11,15 @@ from django.http import JsonResponse
 
 # Create your views here.
 def jsscraper(var1,var2,var3):
-	chrome_options = Options()
-	chrome_options.add_argument('--headless')
-	chrome_options.add_argument('--no-sandbox')
-	chrome_options.add_argument('--disable-dev-shm-usage')
-	chrome_options.add_argument('--disable-gpu')
-	chrome_options.setExperimentalOption("useAutomationExtension", false)
-	browser = webdriver.Chrome(chrome_options=chrome_options)
+	capabilities = {
+		'browserName': 'chrome',
+		'chromeOptions': {
+			'useAutomationExtension': False,
+			'forceDevToolsScreenshot': True,
+			'args': ['--start-maximized', '--disable-infobars']
+		}
+	}
+	browser = webdriver.Chrome(desired_capabilities=capabilities)
 	browser.get(var1)
 	delay = 10 # seconds 
 	try:
