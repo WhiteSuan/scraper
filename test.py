d6bed959f186cde4e53419aa30cac48202b60ea0
whitenguyen <whitenguyen2512@gmail.com>
2018-10-10 11:48:44 +0700

    ver2.0

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException,WebDriverException
 import sys
 
-browser = webdriver.Chrome()
+options = Options()
+options.add_argument("start-maximized");
+options.add_argument("disable-infobars");
+options.add_argument("--disable-extensions");
+options.add_argument("--disable-gpu");
+options.add_argument("--disable-dev-shm-usage");
+options.add_argument("--no-sandbox");
+browser = webdriver.Chrome('/app/.apt/usr/bin/google-chrome',chrome_options=options)
 browser.get("https://www.adairs.com.au/furniture/bedheads/")
 delay = 10 # seconds 
 try:
