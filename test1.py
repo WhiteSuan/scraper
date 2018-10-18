b165cca35603c38abe8ab8b55ba21aa3db4460f4
whitenguyen <whitenguyen2512@gmail.com>
2018-10-18 13:06:27 +0700

    ver4.1

---
+++ test1.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from w3lib.http import basic_auth_header
+
+proxy = "proxy.crawlera.com:8010"
+username = "fcb973633f45443cb0999a782ac6f286"
+password = ""
+
+service_args = [
+	"--ignore-ssl-errors=true",
+	"--ssl-protocol=any",
+	"--proxy={}".format(proxy),
+	"--proxy-type=http",
+]
+
+caps = DesiredCapabilities.PHANTOMJS
+authentication_token = basic_auth_header(username, password).decode('utf-8')
+caps['phantomjs.page.customHeaders.Proxy-Authorization'] = authentication_token
+
+driver = webdriver.PhantomJS(
+	service_args=service_args,
+	desired_capabilities=caps
+)
+driver.get("https://www.empiretoday.com/")
+html = driver.page_source
+driver.close()
+print(html)
